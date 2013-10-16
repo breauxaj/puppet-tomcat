7dcd08d0217777b813b255eb7648872af63bd2f6
Adam Breaux <breauxaj@gmail.com>
2013-10-16 12:54:52 -0400

    running out of ideas

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,3 @@
 class tomcat {
-  file { '/etc/init.d/tomcat':
-    ensure => present,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-    source => 'puppet:///modules/tomcat/tomcat.init'
-  }
 
 }
--- manifests/service.pp
+++ manifests/service.pp
@@ -12,4 +12,12 @@ define tomcat::service (
     require => File['/etc/init.d/tomcat'],
   }
 
+  file { '/etc/init.d/tomcat':
+    ensure => present,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+    source => 'puppet:///modules/tomcat/tomcat.init'
+  }
+
 }
