c57030a6f34f0b883ca172580fca09ff7a4f4b79
Adam Breaux <breauxaj@gmail.com>
2013-11-05 12:52:47 -0500

    manage users

---
+++ manifests/users.pp
@@ -0,0 +1,13 @@
+define tomcat::users (
+  $users = '',
+  $catalina_home = '',
+) {
+  file { "${catalina_home}/conf/tomcat-users.xml":
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('tomcat/tomcat-users.erb'),
+  }
+
+}
